54c674689b1a712a1dda2bd0bde5675670935f06
Dietmar Schlabertz <mail@Dietmar-Schlabertz.de>
2020-09-29 13:47:43 +0200

    29.9.20, spielfähig ohne Animation

--- gameBoard.js
+++ gameBoard.js
@@ -16,16 +16,17 @@ function createShuffleArr(){
 function sortArrayByIndex (tempArr,intInd) {   // Array und Index
     // const newArr = tempArr.slice();
     // const newArr=[...tempArr];
-    newArr=JSON.parse(JSON.stringify(tempArr));
+    const newArr=JSON.parse(JSON.stringify(tempArr)); //Kopie des Array mit neuen Referenzen
     let modified=true;
     while (modified){
         modified=false;
         for(let j=0;j<newArr.length-1;j++) {
             let arrClb=[];
             
-            if (newArr[j][intInd] > newArr[(j+1)][intInd]) {
-                arrClb=newArr[j];
-                newArr[j]=newArr[j+1];
+            if (newArr[j][intInd] > newArr[(j+1)][intInd]) {  //newArr[j][intInd] > newArr[(j+1)][intInd]
+                                                            //Die Zufallszahlen der 
+                arrClb=newArr[j];                       //Die Zeilen werden getauscht
+                newArr[j]=newArr[j+1];                 //dafür bracht man einen Zwischenspeicher
                 newArr[j+1]=arrClb;
                 modified=true;
             }
@@ -37,10 +38,10 @@ function sortArrayByIndex (tempArr,intInd) {   // Array und Index
 
 
 function setImages(tempArray){
-    // const newArray=tempArray.slice();
+    // const newArray=tempArray.slice(); funkioniert nur für das äußere Array, die inneren Arrays bleiben gleich
     // const newArray=[...tempArray];
     const newArray=JSON.parse(JSON.stringify(tempArray));
-    let j=0;
+    let j=0;   //Zählvariable für den Durchlauf des Array
     for (let i=1; i<=(numCards/2); i++){
         
         newArray[j][2]="img"+i;   //Jeweils 2 Felder bekommen das gleiche Bild
@@ -52,22 +53,6 @@ function setImages(tempArray){
 }
 
 function createMasterArray(){
-    //Detailliert===========================================================================================
-    
-    // const initArray=createShuffleArr();
-    //     console.log({initArray});
-    // const shuffledArray = sortArrayByIndex(initArray,1);   //Sortieren nach Zufallszahl
-    //     console.log({shuffledArray});
-    // const imgShuffledArray = setImages(shuffledArray);
-    //     console.log({imgShuffledArray})
-    // const masterArr= sortArrayByIndex(imgShuffledArray,0);   //Ursprüngliche Sortierung 
-    //     console.log({masterArr});
-    // return masterArr;
-    //======================================================================================================
-    // const masterArr=sortArrayByIndex(setImages(sortArrayByIndex(createShuffleArr(),1)),0);
-    // console.log({masterArr});
-    // return masterArr
-    //=======================================================================================================
     return sortArrayByIndex( setImages( sortArrayByIndex( createShuffleArr() ,1) ) ,0);
 }
 
@@ -91,39 +76,27 @@ function createBoard(){
     
     const masterArray=createMasterArray();
     const board = document.querySelector('#board');
-    // console.log({board});
     
-    // board.append(createSubDiv('Dietmar','Superklasse Megaklasse Klasse', 'data-img','img345'));
-    
-    // board.append(createSub('div','Bernhard','Superklasse Megaklasse Klasse'));
-    // board.append(createSub('div','','Superklasse Megaklasse Klasse'));
-    // board.append(createSub('div','OhneDataImg','','','data-img'));
-    // board.append(createSub('div','','','data-img','img787'));
-    // board.append(createSub('div'));
-    
-    
-    let k=0;    //Zählvariable für das Auslesen des masterArray
-    let l=0;    //Zählvariable für die Anzahl der Felder
+    let k=0;    //Zählvariable für das Auslesen des masterArray (24 Kartenfelder von 1 bis 24 (Array:0 bis 23))
+    let l=0;    //Zählvariable für die Anzahl der Felder (25 Felder, 24 Kartenfelder und 1 Score-Feld in der Mitte
     for(let i=0; i<numRows;i++){
-        const rowDiv = createSubDiv('','row');
+        const rowDiv = createSubDiv(('row'+(i+1)),'row');
         for(let j=0; j<numColumns;j++){
             // const cardDiv = document.createElement('div');
             let cardDiv;
             let innerBox;
             if (l===numCards/2){
                 cardDiv=(createSubDiv('score', 'card'));
-                    innerBox=createSubDiv();
+                    innerBox=createSubDiv('','innerBox','','','Score');
                     cardDiv.append(innerBox);               // cardDiv.id="score";
                 l++;
             } else {
-                cardDiv=createSub('div',masterArray[k][0],"card validCard covered","data-img",masterArray[k][2]);
+                cardDiv=createSubDiv(masterArray[k][0],"card validCard","data-img",masterArray[k][2]);
                     innerBox=createSubDiv('','innerBox');
                         let front=createSubDiv('',('front '+masterArray[k][2]))
                         let back=createSubDiv('','back');
                     innerBox.append(front,back);
                 cardDiv.append(innerBox);
-                // cardDiv.id=masterArray[k][0];
-                // cardDiv.classList.add(masterArray[k][2]); 
                 k++;  
                 l++;  
             }
--- play.js
+++ play.js
@@ -1,28 +1,31 @@
 let openCards=0;
-
-
+let card1;
+let card2;
 const board=document.querySelector('#board');
 // console.log(board);
 board.addEventListener('click',function(e){
-    let card1;
-    let card2;
     let card;
+    function restoreCards(){
+        card1.classList.remove('visible');
+        card2.classList.remove('visible');
+        openCards=0;
+    }
+    
     function clickedCard(){
-        console.log(e.target);
-        back=e.target;
+        let back=e.target;
+        console.log({back});
         // back.classList.add('hidden');
         card=back.parentElement.parentElement;
-        if (!(card.classList.contains('validCard'))||(card.classList.contains('visible'))){
+        console.log({card});
+        if (!(card.classList.contains('validCard'))||(card.classList.contains('visible'))||(openCards>1)){
             console.log("Invalid Card");
             return;
         };
-        // console.log(card);
         card.classList.add('visible');
-        console.log('Vorher: ',openCards);
         openCards++;
-        console.log('Nachher: ',openCards);
         return card;
-    }
+    }  //Ende der Funktion clickedCard()
+    
     card=clickedCard();
     
     if (!card){
@@ -31,9 +34,21 @@ board.addEventListener('click',function(e){
     }
     
     if (openCards===1){
+        
         card1=card;
+        
     } else if(openCards===2){
         card2=card;
+        console.log('card1 :',card1.dataset.img)
+        console.log('card2 :',card2.dataset.img)
+        if (card1.dataset.img === card2.dataset.img){   //Hurra, ein Paar
+            card1.classList.remove('validCard');
+            card2.classList.remove('validCard');
+            openCards=0;
+        } else {                                        //Du Loser, wieder nix
+            setTimeout(restoreCards,1500)
+        }
+
     } else {
         console.log('Es sind schon zwei Karten gezogen', openCards);
     }
